5d6a81cfb82b7d5f1bd22819568b0a3a24f5bae6
sovlookup <gonorth@qq.com>
2026-02-05 21:35:08 +0800

    add nfsq

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-魔戒订阅按 Aethersailor 规则转换服务
+多订阅按 Aethersailor 规则转换服务
+
+支持的机场：
+- 魔戒
+- NFSQ
--- index.ts
+++ index.ts
@@ -6,9 +6,13 @@ export default new Elysia()
   .get(
     "/mojie",
     async ({
-      query: { email, password, cvtHost, target, mojieHost, ruleTemplate },
+      query: { email, password, cvtHost, target, mojieHost, ruleTemplate, nfsqHost },
     }) => {
-      const dataurl = `https://${mojieHost}/api/?action=login&email=${encodeURIComponent(email)}&password=${encodeURIComponent(password)}`;
+      const eemail = encodeURIComponent(email);
+      const epassword = encodeURIComponent(password);
+
+      // 解析魔戒 token
+      const dataurl = `https://${mojieHost}/api/?action=login&email=${eemail}&password=${epassword}`;
       const { data: auth } = await ky.get(dataurl).json<{ data: string }>();
       const tipsurl = `https://${mojieHost}/api?action=gettipsbody`;
       const res = await ky
@@ -21,24 +25,52 @@ export default new Elysia()
 
       // 提取 ?token= 后面的 " 前面的内容
       const token = res.match(/\?token=(.*?)"/)?.[1];
+      const mojieSubURL = `https://${mojieHost}/api/v1/client/subscribe?token=${token}`;
+
+      // 解析 nfsq
+      const resn = await ky.get(`https://${nfsqHost}`)
+      const b = await resn.text();
+      const matches = b.match(/src="(\/slide_check_.*?\.js)">/);
+      const slidePath = matches ? matches[1] : null;
+      const slideRes = await ky.get(`https://${nfsqHost}${slidePath}`)
+      const slideText = await slideRes.text();
+      const tokenMatch = slideText.match(/set_access_token-[^"']+/)!;
+      const nfsqSubRes = await ky.get(`https://${nfsqHost}/${tokenMatch[0]}`)
+      const cookie = nfsqSubRes.headers.get("set-cookie")!.split(";")[0]
+      const nfsqRes = await ky.post(`https://${nfsqHost}/api/v1/passport/auth/login`, {
+        headers: {
+          "content-type": "application/x-www-form-urlencoded",
+          cookie: cookie,
+        },
+        body: `email=${eemail}&password=${epassword}`
+      }).json<{ data: { auth_data: string } }>();
+      const a = await ky.get(`https://${nfsqHost}/api/v1/user/getSubscribe`, {
+        headers: {
+          "cookie": cookie,
+          authorization: nfsqRes.data.auth_data,
+        }
+      }).json<{ data: { subscribe_url: string } }>()
+      const subURL = a.data.subscribe_url
 
       const suburl = encodeURIComponent(
-        `https://${mojieHost}/api/v1/client/subscribe?token=${token}`,
+        [mojieSubURL, subURL].join("|"),
       );
 
       const configurl = encodeURIComponent(ruleTemplate);
-
       const url = `https://${cvtHost}/sub?target=${target}&new_name=true&url=${suburl}&config=${configurl}&emoji=true&list=false&sort=true&udp=true&append_type=true&classic=true`;
 
-      return await ky.get(url).text();
+      return await ky.get(url, {
+        timeout: 30000,
+      }).text();
     },
     {
       query: z.object({
         email: z.string(),
         password: z.string(),
-        cvtHost: z.string(),
+        cvtHost: z.string().default("mxpwrfkgqoys.ap-northeast-1.clawcloudrun.com"),
         target: z.optional(z.string()).default("clash"),
         mojieHost: z.optional(z.string()).default("mojie.xn--yrs494l.com"),
+        nfsqHost: z.optional(z.string()).default("www.nfsqttt.com"),
         ruleTemplate: z
           .optional(z.string())
           .default(
